7aef0eb09df998e175659cd438147bbcfa9927b4
kevinD006 <kevinacevedo060@gmail.com>
2024-08-13 19:35:27 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const btncart = document.querySelector('.container-icon')
+const containerCartproducts = document.querySelector('.container-cart-products')
+
+
+btncart.addEventListener('click', () => {
+    containerCartproducts.classList.toggle
+    ('hidden-cart')
+
+
+})
